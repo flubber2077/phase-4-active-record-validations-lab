72a89b7537297062dfd1ec72bced93bb1c8937e2
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-09 17:19:12 -0400

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction)}
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [\d]" || "Guess")
+            errors.add(:title, "Your title won't get any clicks!")
+        end
+    end
+
 end
+
